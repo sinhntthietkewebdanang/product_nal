0c824063437e86d3e73d0d661d2ee0e1b449e1c8
sinh <sinhnt@thietkewebdanang.com>
2017-08-08 05:14:15 +0700

    kkk

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -11,6 +11,6 @@ class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
     function test(){
-    	echo 'test confilic';
+    	echo 'test confilic1abcd';
     }
 }
